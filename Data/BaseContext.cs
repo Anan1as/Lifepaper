983f423763eabe1c74c964e48a989a7f93c3583d
kemtch19 <kemtch19@gmail.com>
2024-07-10 19:37:52 -0500

    feat: create usuarios model

--- Data/BaseContext.cs
+++ Data/BaseContext.cs
@@ -5,8 +5,7 @@ namespace Lifepaper.Data
 {
     public class BaseContext : DbContext
     {
-        public BaseContext(DbContextOptions<BaseContext> options): base(options)
-        {
-        }
+        public BaseContext(DbContextOptions<BaseContext> options): base(options){}
+        public DbSet<Usuario> Usuarios {get; set;}
     }
 }
---
+++ Models/Usuario.cs
@@ -0,0 +1,12 @@
+namespace Lifepaper.Models{
+    public class Usuario{
+        public int UserId { get; set;}
+        public string Nombre { get; set;}
+        public string Apellido { get; set;}
+        public string Correo { get; set;}
+        public string Contraseña { get; set;}
+        public DateTime FechaRegistro { get; set;}
+        public string GoogleId { get; set;}
+        public string Nacionalidad {get;set;}
+    }
+}
