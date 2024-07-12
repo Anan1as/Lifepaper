85759b594c0f7b9a6100b2b81af1cd65a1797432
leandroVR1 <yeifry.121@gmail.com>
2024-07-12 17:58:25 -0500

    feat: Implement password recovery functionality

--- obj/Debug/net8.0/Lifepaper.AssemblyInfo.cs
+++ obj/Debug/net8.0/Lifepaper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lifepaper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c89732a079864ae207c93b44df674a0a8441989b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3e108574296524fb50bbde10723178a3ffc35c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lifepaper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lifepaper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
